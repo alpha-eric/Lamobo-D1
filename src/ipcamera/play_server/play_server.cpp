db506a267f24b63b9780efa78fcc4d2ce51b021d
eric hsieh <eric_hsieh@alphanetworks.com>
2016-04-10 14:50:09 +0800

    remove fprintf.

--- src/ipcamera/play_server/play_server.cpp
+++ src/ipcamera/play_server/play_server.cpp
@@ -64,13 +64,13 @@ static void notify_handler(int conn)
 void create_notify_thread(int conn)
 {
 	pthread_t pid;
-        pthread_attr_t attr;
-        pthread_attr_init(&attr);
-        pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
-        size_t stackSize = 8*1024;
-        pthread_attr_setstacksize(&attr, stackSize);
-        pthread_create(&pid, &attr, (void *(*)(void *)) notify_handler, (void *)conn);
-        pthread_attr_destroy(&attr);	
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+	size_t stackSize = 8*1024;
+	pthread_attr_setstacksize(&attr, stackSize);
+	pthread_create(&pid, &attr, (void *(*)(void *)) notify_handler, (void *)conn);
+	pthread_attr_destroy(&attr);	
 }
 
 int progress_connection(int conn)
@@ -81,15 +81,14 @@ int progress_connection(int conn)
 	int volume = 80;
 
 	int ret = 0;
-    int	len = 0;
+	int	len = 0;
 	T_U8 *buf = NULL;
 	CAudioSink play;
-	int max_buf_size = (sample_rate * channels * sample_bits);
+	int max_buf_size = (sample_rate * channels * sample_bits) / 8;
 	fprintf(stderr, "prepare buf %d\n", max_buf_size);
 	create_notify_thread(conn);
 	do {
-		buf = 
-		  (T_U8 *)malloc(max_buf_size);
+		buf = (T_U8 *)malloc(max_buf_size);
 		if (!buf) {
 			fprintf(stderr, "alloc buffer failed.\n");
 			break;
@@ -102,17 +101,25 @@ int progress_connection(int conn)
 			break;
 		}
 		play.start();
-		int count = 0;
-		while (!connection_exit && (len = recv(conn, buf, max_buf_size, 0)) > 0) {
-			fprintf(stderr, "read %d\n", len);
-			count += len;
+		while (!connection_exit ) {
+			len = recv(conn, buf, max_buf_size, MSG_WAITALL);
+			if (len == 0) {
+				fprintf(stderr, "client close this connection\n");
+				break;
+			} else if (len < 0) {
+				if (errno == EAGAIN || errno == EWOULDBLOCK) {
+					fprintf(stderr, "recv was interrupted by signal, do again\n");
+					continue;
+				} else {
+					fprintf(stderr, "error: %s\n", strerror(errno));
+					break;
+				}
+			}
 			ret = play.render(buf, len);
 			if (ret != 0) {
 				fprintf(stderr, "render data failed.\n");
-				break;
 			}
 		}
-		fprintf(stderr, "total: %d\n", count);
 	} while (false);
 	if (play.isOpened()) {
 		play.stop();
@@ -128,24 +135,24 @@ int progress_connection(int conn)
 
 int setupV4(const int port)
 {
-     int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-     struct sockaddr_in serv_addr;
-     memset((unsigned char *)&serv_addr, 0x00, sizeof(serv_addr));
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_port = htons(port);
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
+	int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	struct sockaddr_in serv_addr;
+	memset((unsigned char *)&serv_addr, 0x00, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(port);
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
 
-     if(bind(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
-          printf("bind4 failed: %s\n", strerror(errno));
-          close(sock);
-          return -1;
-     }
-     if(listen(sock, 1) < 0){
-          printf("listen failed: %s\n", strerror(errno));
-          close(sock);
-          return -1;
-     }
-     return sock;
+	if(bind(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
+		printf("bind4 failed: %s\n", strerror(errno));
+		close(sock);
+		return -1;
+	}
+	if(listen(sock, 1) < 0){
+		printf("listen failed: %s\n", strerror(errno));
+		close(sock);
+		return -1;
+	}
+	return sock;
 }
 
 int main(int argc, const char *argv[])
@@ -154,28 +161,28 @@ int main(int argc, const char *argv[])
 
 	char *buffer = NULL;
 	int sock4 = setupV4(port);
-    do{
-        fd_set rset;
-        FD_ZERO(&rset);
-        FD_SET(sock4, &rset);
-        int maxfd = sock4 + 1;
-        if(select(maxfd, &rset, NULL, NULL, 0) > 0){
+	do{
+		fd_set rset;
+		FD_ZERO(&rset);
+		FD_SET(sock4, &rset);
+		int maxfd = sock4 + 1;
+		if(select(maxfd, &rset, NULL, NULL, 0) > 0){
 			int conn = 0;
-            if(FD_ISSET(sock4, &rset)){
+			if(FD_ISSET(sock4, &rset)){
 				struct sockaddr_in client_addr;    
-                socklen_t len = sizeof(client_addr);
-                conn = accept(sock4, (struct sockaddr *)&client_addr, &len);    
+				socklen_t len = sizeof(client_addr);
+				conn = accept(sock4, (struct sockaddr *)&client_addr, &len);    
 				fprintf(stderr, "Got a ipv4 connection\n");
-            }else{
-                continue;
-            }
-	    connection_exit = false;
-	    progress_connection(conn);
-            close(conn);
-	    fprintf(stderr, "connection is close\n");
-	}else{
-            continue;
-        }
+			}else{
+				continue;
+			}
+			connection_exit = false;
+			progress_connection(conn);
+			close(conn);
+			fprintf(stderr, "connection is close\n");
+		}else{
+			continue;
+		}
 	}while(true);
 	if (sock4 > 0) {
 		close(sock4);
